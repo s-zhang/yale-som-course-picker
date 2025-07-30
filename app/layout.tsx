ba387653a205f6f176748ad869b2a53a75512f9b
Pingshan Zhang <szh10000@gmail.com>
2025-07-29 23:31:14 -0400

    Remove header and update footer message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { Toaster } from "@/components/ui/toaster"
-import Header from "@/components/header"
-import Footer from "@/components/footer"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import Footer from "@/components/footer";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
   display: "swap",
-})
+});
 
 export const metadata: Metadata = {
   title: "SOM Course Picker",
@@ -19,21 +18,20 @@ export const metadata: Metadata = {
   icons: {
     icon: "https://som.yale.edu/themes/custom/som/images/favicons/favicon.ico",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={inter.variable}>
       <body className={inter.className}>
-        <Header />
         {children}
         <Footer />
         <Toaster />
       </body>
     </html>
-  )
+  );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,27 +1,32 @@
-import type React from "react"
+import type React from "react";
 
 export default function Footer() {
   return (
-    <footer className="py-4 text-center text-sm text-muted-foreground">
-      Made by{' '}
-      <a
-        href="https://www.linkedin.com/in/pingshan-zhang"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="underline"
-      >
-        Pingshan Zhang, MBA
-      </a>
-      . Support me by{' '}
-      <a
-        href="https://github.com/s-zhang/yale-som-course-picker"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="underline"
-      >
-        starring this repo on GitHub
-      </a>
-      .
+    <footer className="py-4 text-center text-sm text-muted-foreground space-y-1 sm:space-y-0">
+      <span className="block sm:inline">
+        Make by{" "}
+        <a
+          href="https://www.linkedin.com/in/pingshan-zhang"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          Pingshan Zhang, MBA '26
+        </a>
+        .
+      </span>{" "}
+      <span className="block sm:inline">
+        Support me by{" "}
+        <a
+          href="https://github.com/s-zhang/yale-som-course-picker"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          starring this repo on GitHub
+        </a>{" "}
+        :)
+      </span>
     </footer>
-  )
+  );
 }
